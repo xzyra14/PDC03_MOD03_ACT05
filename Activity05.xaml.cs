f9fa1066ea6cfec51454207b70acd559be01c2d6
Xzyra Lapid <83936947+xzyra14@users.noreply.github.com>
2022-05-11 23:12:41 +0800

    Add files via upload

---
+++ Activity05.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity05 : ContentPage
+    {
+        public Activity05()
+        {
+            InitializeComponent();
+        }
+    }
+}
